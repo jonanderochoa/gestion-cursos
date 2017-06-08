121887cf1fd51efdd365af8d16a9081ebf250125
Jon Ander <jonanderochoa@gmail.com>
2017-06-08 17:17:23 +0200

    Modificado el root-context.xml, acabado CursoServiceImp y empezado CursoController

---
+++ src/main/java/com/ipartek/formacion/controller/CursoController.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.ipartek.formacion.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Controlador que se comunica entre la vista y la capa service de cursos
+ * @author Jon Ander Ochoa Ruiz
+ * 8 de jun. de 2017
+ */
+@Controller
+@RequestMapping(value="/cursos")
+public class CursoController {
+
+	
+}
--- src/main/java/com/ipartek/formacion/service/CursoServiceImp.java
+++ src/main/java/com/ipartek/formacion/service/CursoServiceImp.java
@@ -3,10 +3,58 @@
  */
 package com.ipartek.formacion.service;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.ipartek.formacion.dbms.dao.interfaces.CursoDAO;
+import com.ipartek.formacion.dbms.persistence.Curso;
+import com.ipartek.formacion.service.interfaces.CursoService;
+
 /**
+ * Clase encargada de implementar las funcionalidades creadas en la interfaz CursoService
  * @author Jon Ander Ochoa Ruiz
  * 8 de jun. de 2017
  */
-public class CursoServiceImp {
+@Service
+public class CursoServiceImp implements CursoService {
+
+	@Inject
+	private CursoDAO cursoDAO;
+	private static final Logger LOGGER = LoggerFactory.getLogger(CursoServiceImp.class);
+	
+	@Override
+	public Curso create(Curso curso) {
+		LOGGER.info("createService");
+		return cursoDAO.create(curso);
+	}
+
+	@Override
+	public List<Curso> getAll() {
+		LOGGER.info("getAllService");
+		return cursoDAO.getAll();
+	}
+
+	@Override
+	public Curso getById(int codigo) {
+		LOGGER.info("getByIdService");
+		return cursoDAO.getById(codigo);
+	}
+
+	@Override
+	public Curso update(Curso curso) {
+		LOGGER.info("updateService");
+		return cursoDAO.update(curso);
+	}
+
+	@Override
+	public void delete(int codigo) {
+		LOGGER.info("deleteService");
+		cursoDAO.delete(codigo);
+	}
 
 }
